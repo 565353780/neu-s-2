04ac9327544d02c7d46a8df872f8544f968d61ac
chLi <565353780@qq.com>
2023-09-28 00:52:01 +0800

    set to auto install vulkan-sdk

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@ cd ..
 git clone https://github.com/565353780/colmap-manage.git
 git clone --recursive https://github.com/19reborn/NeuS2 ns2
 
+wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list https://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
+sudo apt update
+sudo apt install vulkan-sdk
+
 sudo apt-get install build-essential git python3-dev python3-pip libopenexr-dev libxi-dev \
 	libglfw3-dev libglew-dev libomp-dev libxinerama-dev libxcursor-dev
 
