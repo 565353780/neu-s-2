6a95702a6b63d474d4e3b1489a8219ca9b2316e6
chLi <565353780@qq.com>
2023-09-27 22:47:31 +0800

    finish compile neus2

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# neus2
+# NeuS2
+
+## Source
+
+```bash
+https://github.com/19reborn/NeuS2
+```
+
+## Requirements
+
+```bash
+gcc-11 & g++-11
+cuda-12.2
+optix-7.5.0
+```
+
+and set
+
+```bash
+~/.zshrc
+->
+export OptiX_INSTALL_DIR=$HOME/NVIDIA-OptiX-SDK-8.0.0-linux64-x86_64/
+```
+
+## Install
+
+```bash
+conda create -n ns2 python=3.9
+conda activate ns2
+./setup.sh
+```
+
+## Run
+
+```bash
+python convert.py
+python demo.py
+```
+
+## Enjoy it~
---
+++ dev_setup.sh
@@ -0,0 +1,17 @@
+cd ..
+git clone git@github.com:565353780/colmap-manage.git
+git clone --recursive https://github.com/19reborn/NeuS2 ns2
+
+sudo apt-get install build-essential git python3-dev python3-pip libopenexr-dev libxi-dev \
+	libglfw3-dev libglew-dev libomp-dev libxinerama-dev libxcursor-dev
+
+cd colmap-manage
+./dev_setup.sh
+
+cd ../ns2
+rm -rf build
+
+pip install -r requirements.txt
+
+cmake . -B build
+cmake --build build --config RelWithDebInfo -j
